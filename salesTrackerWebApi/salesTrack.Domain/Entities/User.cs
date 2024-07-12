05598fd7dc1891f7f7cb97f190589dbdbad75dee
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-07-12 14:49:13 +0530

    user model created

--- salesTrackerWebApi/salesTrack.Domain/Entities/User.cs
+++ salesTrackerWebApi/salesTrack.Domain/Entities/User.cs
@@ -1,4 +1,5 @@
-﻿using SalesTrack.Domain.Shared;
+﻿using salesTrack.Domain.Enums;
+using SalesTrack.Domain.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +10,15 @@ namespace SalesTrack.Domain.Entities
 {
     public  class User:BaseModel
     {
+        public string? Name { get; set; }
         public string? UserName { get; set; }
         public string? Email { get; set; }
         public string? ContactNo { get; set; }
+        public string? Password { get; set; }
+        public int Salt { get; set; }
+        public UserRole UserRole { get; set; }
+        public int ResetCode { get; set; }
+        public DateTimeOffset? ResetExpiry { get; set; }
+        public bool IsPasswordTemporary { get; set; } = true;
     }
 }
