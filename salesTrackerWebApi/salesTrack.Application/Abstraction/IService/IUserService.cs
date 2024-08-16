601d1ce81e63f2c78c390cf213a11b261451207d
Abid <abidrather36@gmail.com>
2024-08-16 11:09:52 +0530

    updated

--- salesTrackerWebApi/salesTrack.Application/Abstraction/IService/IUserService.cs
+++ salesTrackerWebApi/salesTrack.Application/Abstraction/IService/IUserService.cs
@@ -1,13 +1,4 @@
-﻿using salesTrack.Domain.Models;
-using SalesTrack.Application.Common;
-using SalesTrack.Domain.Entities.Models.Request;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SalesTrack.Application.Abstraction.IService
+﻿namespace SalesTrack.Application.Abstraction.IService
 {
     public interface IUserService
     {
