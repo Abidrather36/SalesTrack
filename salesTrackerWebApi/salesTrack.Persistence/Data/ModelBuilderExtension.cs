1fa28e524544404adb900a65fd24e0dc906583c9
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-08-19 11:51:02 +0530

    seedUser added

---
+++ salesTrackerWebApi/salesTrack.Persistence/Data/ModelBuilderExtension.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using salesTrack.Application.Utils;
+using salesTrack.Domain.Enums;
+using SalesTrack.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salesTrack.Persistence.Data
+{
+   
+        public static class ModelBuilderExtensions
+        {
+            public static void SeedUsers(this ModelBuilder modelBuilder)
+            {
+              var salt = AppEncryption.GenerateSalt();
+            var dbPassword = AppEncryption.CreatePassword("ramrk@123", salt);
+                modelBuilder.Entity<User>().HasData(new User
+                {
+                    Id = Guid.NewGuid(), 
+                    Name = "Ram",
+                    Email = "ramrk@anterntech.com",
+                    ContactNo = "6545454543",
+                    Address = "123 Main St hebbal",
+                    PostalCode = "12345",
+                    Password = dbPassword,
+                    Salt = salt,
+                    UserRole = UserRole.Admin,
+                    ResetCode = 12345,
+                    ResetExpiry = DateTimeOffset.UtcNow.AddMinutes(15),
+                    IsPasswordTemporary = true
+                });;
+
+         
+            }
+        }
+
+    }
--- salesTrackerWebApi/salesTrack.Persistence/Migrations/20240819062014_seedValueAddedAgain.Designer.cs
+++ salesTrackerWebApi/salesTrack.Persistence/Migrations/20240819062014_seedValueAddedAgain.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SalesTrack.Persistence.Data;
 
@@ -11,9 +12,11 @@ using SalesTrack.Persistence.Data;
 namespace salesTrack.Persistence.Migrations
 {
     [DbContext(typeof(SalesTrackDBContext))]
-    partial class SalesTrackDBContextModelSnapshot : ModelSnapshot
+    [Migration("20240819062014_seedValueAddedAgain")]
+    partial class seedValueAddedAgain
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
