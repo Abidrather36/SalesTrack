a147cea32c873135f8374477437159110d09620b
AAMIR BASHIR <112857472+amykash80@users.noreply.github.com>
2024-08-19 09:56:13 +0530

    userToken.cs implemented

---
+++ salesTrackerWebApi/salesTrack.Domain/Models/JWT/UserTokens.cs
@@ -0,0 +1,60 @@
+﻿using salesTrack.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace salesTrack.Domain.Models.JWT
+{
+    public class UserTokens
+    {
+       
+            public string? Token
+            {
+                get;
+                set;
+            }
+            public string? UserName
+            {
+                get;
+                set;
+            }
+            public UserRole? UserRole
+            {
+                get;
+                set;
+            }
+            public string? FullName
+            {
+                get;
+                set;
+            }
+            public TimeSpan Validaty
+            {
+                get;
+                set;
+            }
+            public string? RefreshToken
+            {
+                get;
+                set;
+            }
+            public Guid? Id
+            {
+                get;
+                set;
+            }
+            public string? EmailId
+            {
+                get;
+                set;
+            }
+            
+            public DateTime ExpiredTime
+            {
+                get;
+                set;
+            }
+    }
+}
